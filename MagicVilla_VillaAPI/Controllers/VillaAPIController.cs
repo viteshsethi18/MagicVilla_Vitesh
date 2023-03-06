49cf85fd22fd36786d4da48cc1c826db63bc3daf
viteshsethi18 <500076357@stu.upes.ac.in>
2023-03-06 13:16:10 +0530

    DTO implementation

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,4 +1,5 @@
 ﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
@@ -8,12 +9,12 @@ namespace MagicVilla_VillaAPI.Controllers
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<Villa> GetVillas()
+        public IEnumerable<VillaDTO> GetVillas()
         {
-            return new List<Villa>
+            return new List<VillaDTO>
             {
-                new Villa{Id=1, Name="Pool View"},
-                new Villa{Id=2, Name="Beach View"}
+                new VillaDTO{Id=1, Name="Pool View"},
+                new VillaDTO{Id=2, Name="Beach View"}
             };
         }
     }
