7d797806e524c30c708009409d30daeb2b2718b1
viteshsethi18 <500076357@stu.upes.ac.in>
2023-03-06 13:52:12 +0530

    individual access

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,4 +1,5 @@
-﻿using MagicVilla_VillaAPI.Models;
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
 using MagicVilla_VillaAPI.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,11 +12,12 @@ namespace MagicVilla_VillaAPI.Controllers
         [HttpGet]
         public IEnumerable<VillaDTO> GetVillas()
         {
-            return new List<VillaDTO>
-            {
-                new VillaDTO{Id=1, Name="Pool View"},
-                new VillaDTO{Id=2, Name="Beach View"}
-            };
+            return VillaStore.villaList;
+        }
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u=>u.Id==id);
         }
     }
 }
